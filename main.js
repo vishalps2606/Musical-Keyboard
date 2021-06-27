c7892457a0e48a76cbb89afefb14481f5405d2d9
vishalps2606 <70359874+vishalps2606@users.noreply.github.com>
2021-06-27 11:10:34 +0530

    Resolve the issue happening by that bastard quotation mark

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key = "${e.keyCode}]`);
+  const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
   if (!audio) return;
 
-  // key.classList.add("playing");
+  key.classList.add("playing");
   audio.currentTime = 0;
   audio.play();
 }
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
-  // e.target.classList.remove("playing");
+  e.target.classList.remove("playing");
 }
 
 const keys = Array.from(document.querySelectorAll(".key"));
